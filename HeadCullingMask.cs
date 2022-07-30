68ddfb3d960ba519f02ebe5402c859c9f6c106f9
hantabaru1014 <hantabaru1014@gmail.com>
2022-07-31 04:09:33 +0900

    first

---
+++ HeadCullingMask.cs
@@ -0,0 +1,95 @@
+﻿using NeosModLoader;
+using HarmonyLib;
+using FrooxEngine;
+using FrooxEngine.CommonAvatar;
+using System.Collections.Generic;
+using BaseX;
+using UnityNeos;
+using System.Reflection;
+using UnityEngine;
+using System.Linq;
+
+namespace HeadCullingMask
+{
+    public class HeadCullingMask : NeosMod
+    {
+        public override string Name => "HeadCullingMask";
+        public override string Author => "hantabaru1014";
+        public override string Version => "1.0.0";
+
+        public static readonly string TARGET_COMMENT_TEXT = "net.hantabaru1014.HeadCullingMask.TargetSlot";
+        public static readonly byte USE_LAYER = 10;
+
+        private static readonly FieldInfo forceLayerFieldInfo = AccessTools.Field(typeof(SlotConnector), "forceLayer");
+
+        public override void OnEngineInit()
+        {
+            Harmony harmony = new Harmony("net.hantabaru1014.HeadCullingMask");
+            harmony.PatchAll();
+
+            var headOutputs = Resources.FindObjectsOfTypeAll<HeadOutput>()
+                .Where(c => c.gameObject.hideFlags != HideFlags.NotEditable && c.gameObject.hideFlags != HideFlags.HideAndDontSave);
+            foreach (var headOutput in headOutputs)
+            {
+                foreach (var cam in headOutput.cameras)
+                {
+                    cam.cullingMask &= ~(1 << USE_LAYER);
+                    Msg($"Patch Camera cullingMask : {cam.name}");
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(AvatarRoot), nameof(AvatarRoot.Equip))]
+        class AvatarRootEquipPatch
+        {
+            static void Postfix(AvatarRoot __instance)
+            {
+                Msg("Avatar Equip!");
+                UpdateLayer(__instance.Slot, USE_LAYER, true);
+            }
+        }
+
+        [HarmonyPatch(typeof(AvatarRoot), nameof(AvatarRoot.Dequip))]
+        class AvatarRootDequipPatch
+        {
+            static void Postfix(AvatarRoot __instance)
+            {
+                Msg("Avatar Dequip!");
+                UpdateLayer(__instance.Slot, 0, false);
+            }
+        }
+
+        [HarmonyPatch(typeof(HeadOutput), "Awake")]
+        class HeadOutputAwakePatch
+        {
+            static void Postfix(List<UnityEngine.Camera> ___cameras)
+            {
+                foreach (var cam in ___cameras)
+                {
+                    cam.cullingMask &= ~(1 << USE_LAYER);
+                    Msg($"Patch Camera cullingMask : {cam.name}");
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(CameraPortal), "RenderReflection")]
+        class CameraPortalRenderReflectionPatch
+        {
+            static void Prefix(UnityEngine.Camera reflectionCamera)
+            {
+                reflectionCamera.cullingMask |= (1 << USE_LAYER);
+            }
+        }
+
+        private static void UpdateLayer(Slot avatarRootSlot, byte layer, bool excludeDisable)
+        {
+            List<Comment> list = Pool.BorrowList<Comment>();
+            avatarRootSlot.GetComponentsInChildren<Comment>(list, (Comment c) => c.Text.Value == TARGET_COMMENT_TEXT, excludeDisable, false);
+            foreach (var comment in list)
+            {
+                forceLayerFieldInfo.SetValue((SlotConnector)comment.Slot.Connector, layer);
+                Msg($"SetLayer : {comment.Slot.Name} to {(int)layer}");
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# HeadCullingMask
+
+A [NeosModLoader](https://github.com/zkxs/NeosModLoader) mod for [Neos VR](https://neos.com/) that make your avatar's head invisible only to your FPV.  
+(Looks like NearClip, but others are visible at near distance).  
+
+## How To Use
+- Attach a Comment component to the avatar's face and hair (what you want to hide) slot and set "`net.hantabaru1014.HeadCullingMask.TargetSlot`" to Text.
+- It only works when the Comment component's Enabled is True.
+- It is processed when the avatar is equiped, so if the Comment component is setup while the avatar is on, it must be equip again.
+
+## Installation
+1. Install [NeosModLoader](https://github.com/zkxs/NeosModLoader).
+2. Place [HeadCullingMask.dll](https://github.com/hantabaru1014/HeadCullingMask/releases/latest/download/HeadCullingMask.dll) into your `nml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\NeosVR\nml_mods` for a default install. You can create it if it's missing, or if you launch the game once with NeosModLoader installed it will create the folder for you.
+3. Start the game. If you want to verify that the mod is working you can check your Neos logs.
